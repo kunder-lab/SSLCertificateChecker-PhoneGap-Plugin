af0e427d91ecb0c7d9e45ebe41dcbaca527b3625
Alfredo Vidueira <avidueira@kunder.cl>
2018-06-08 16:17:44 -0400

    Cambios para comprobación por dominio

--- src/ios/SSLCertificateChecker.m
+++ src/ios/SSLCertificateChecker.m
@@ -1,4 +1,5 @@
 #import "SSLCertificateChecker.h"
+#import <openssl/x509.h>
 #import <Cordova/CDV.h>
 #import <Cordova/CDVPluginResult.h>
 #import <CommonCrypto/CommonDigest.h>
@@ -8,7 +9,9 @@
 @property (strong, nonatomic) CDVPlugin *_plugin;
 @property (strong, nonatomic) NSString *_callbackId;
 @property (strong, nonatomic) NSString *_allowedFingerprint;
-@property (nonatomic, assign) BOOL sentResponse;
+@property (nonatomic, assign) BOOL isFingerprintOK;
+@property (nonatomic, assign) BOOL isCommonNameOK;
+@property (nonatomic, assign) NSString *_serverURL;
 
 - (id)initWithPlugin:(CDVPlugin*)plugin callbackId:(NSString*)callbackId allowedFingerprint:(NSString*)allowedFingerprint;
 
@@ -17,7 +20,8 @@
 @implementation CustomURLConnectionDelegate
 
 - (id)initWithPlugin:(CDVPlugin*)plugin callbackId:(NSString*)callbackId allowedFingerprint:(NSString*)allowedFingerprint {
-    self.sentResponse = FALSE;
+    self.isFingerprintOK = FALSE;
+    self.isCommonNameOK = FALSE;
     self._plugin = plugin;
     self._callbackId = callbackId;
     self._allowedFingerprint = allowedFingerprint;
@@ -39,20 +43,54 @@
         NSString* fingerprint = [self getFingerprint:certRef];
         
         if ([fingerprint caseInsensitiveCompare: self._allowedFingerprint] == NSOrderedSame) {
-            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"CONNECTION_SECURE"];
-            [self._plugin.commandDelegate sendPluginResult:pluginResult callbackId:self._callbackId];
-            self.sentResponse = TRUE;
+            self.isFingerprintOK = TRUE;
             break;
         }
+
+        NSData *certificateData = (NSData *) SecCertificateCopyData(certRef);
+        const unsigned char *certificateDataBytes = (const unsigned char *)[certificateData bytes];
+        X509 *certificateX509 = d2i_X509(NULL, &certificateDataBytes, [certificateData length]);
+        NSString *issuerCommonName = CertificateGetIssuerCommonName(certificateX509);
+        if ([issuerCommonName caseInsensitiveCompare: self._serverURL]) {
+            self.isCommonNameOK = TRUE;
+        }
     }
-    
-    if (! self.sentResponse) {
+
+    NSLog(@"sslcheck: %@ %@", self.isFingerprintOK, self.isCommonNameOK);
+
+    if (self.isFingerprintOK && self.isCommonNameOK) {
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"CONNECTION_SECURE"];
+        [self._plugin.commandDelegate sendPluginResult:pluginResult callbackId:self._callbackId];
+    } else {
         CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_JSON_EXCEPTION messageAsString:@"CONNECTION_NOT_SECURE"];
         [self._plugin.commandDelegate sendPluginResult:pluginResult callbackId:self._callbackId];
     }
+}
 
-    NSString *host = [[challenge protectionSpace] host];
-    NSLog(@"host: %@", host);
+- (NSString *) CertificateGetIssuerCommonName(X509 *certificateX509)
+{
+    NSString *issuer = nil;
+    if (certificateX509 != NULL) {
+        X509_NAME *issuerX509Name = X509_get_issuer_name(certificateX509);
+
+        if (issuerX509Name != NULL) {
+            int nid = OBJ_txt2nid("CN"); // commonname
+            int index = X509_NAME_get_index_by_NID(issuerX509Name, nid, -1);
+
+            X509_NAME_ENTRY *issuerNameEntry = X509_NAME_get_entry(issuerX509Name, index);
+
+            if (issuerNameEntry) {
+                ASN1_STRING *issuerNameASN1 = X509_NAME_ENTRY_get_data(issuerNameEntry);
+
+                if (issuerNameASN1 != NULL) {
+                    unsigned char *issuerName = ASN1_STRING_data(issuerNameASN1);
+                    issuer = [NSString stringWithUTF8String:(char *)issuerName];
+                }
+            }
+        }
+    }
+
+    return issuer;
 }
 
 - (NSCachedURLResponse *)connection:(NSURLConnection *)connection willCacheResponse:(NSCachedURLResponse *)cachedResponse {
@@ -107,6 +145,8 @@
     [NSURLCache setSharedURLCache:sharedCache];
 
     NSString *serverURL = [command.arguments objectAtIndex:0];
+    self._serverURL = [serverURL substringToIndex:7];
+    NSLog(@"_serverURL: %@", self._serverURL);
     //NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:serverURL]];
     NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:serverURL] cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:6.0];
 
